02fd659121aa1d3beeae28bad28aa29e3e3fa2b8
Chris Koon <koonchris@ymail.com>
2019-03-29 02:19:11 -0700

    Set up volume controller

---
+++ Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform tf;
+    public Vector3 myRotation;
+
+    private void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion newRotation = Quaternion.Euler(myRotation);
+        tf.rotation = newRotation;
+    }
+}
---
+++ Assets/Scripts/UI/Sound/VolumeSliderController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSliderController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer mixer;
+
+    public void setMasterVolume(float value)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void setMusicVolume(float value)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void setAmbientVolume(float value)
+    {
+        mixer.SetFloat("AmbienceVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void setSoundFXVolume(float value)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
+    }
+}
